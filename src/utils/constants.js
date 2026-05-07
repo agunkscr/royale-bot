bdf519047c344eb1857f5a1b74a2bf8c500e237b
agunkscr <71326079+agunkscr@users.noreply.github.com>
2026-05-07 17:04:55 +0700

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,22 +1,9 @@
-// Action types
-const ACTIONS = {
+export const ACTIONS = {
   MOVE: 'move',
   ATTACK: 'attack',
   PICKUP: 'pickup',
   EQUIP: 'equip',
   USE: 'use_item',
   REST: 'rest',
-  TALK: 'talk',
-  WHISPER: 'whisper',
-  INTERACT: 'interact',
+  DROP: 'drop_item',
 };
-
-// Cooldown group (aksi yang butuh cooldown)
-const COOLDOWN_ACTIONS = [
-  ACTIONS.MOVE, ACTIONS.ATTACK, ACTIONS.USE, ACTIONS.REST, ACTIONS.INTERACT
-];
-
-// Free actions (tidak pengaruhi cooldown)
-const FREE_ACTIONS = [ACTIONS.PICKUP, ACTIONS.EQUIP, ACTIONS.TALK, ACTIONS.WHISPER];
-
-module.exports = { ACTIONS, COOLDOWN_ACTIONS, FREE_ACTIONS };
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,21 +1,15 @@
-const { logLevel } = require('../config');
+import { config } from '../config.js';
 
 const LEVELS = { debug: 0, info: 1, warn: 2, error: 3 };
-const currentLevel = LEVELS[logLevel] || LEVELS.info;
+const currentLevel = LEVELS[config.logLevel] || LEVELS.info;
 
-function timestamp() {
+function ts() {
   return new Date().toISOString();
 }
 
-function log(level, ...args) {
-  if (LEVELS[level] >= currentLevel) {
-    console.log(`[${timestamp()}] [${level.toUpperCase()}]`, ...args);
-  }
-}
-
-module.exports = {
-  debug: (...args) => log('debug', ...args),
-  info: (...args) => log('info', ...args),
-  warn: (...args) => log('warn', ...args),
-  error: (...args) => log('error', ...args),
+export const logger = {
+  debug: (...args) => currentLevel <= 0 && console.log(`[${ts()}] [DEBUG]`, ...args),
+  info: (...args) => currentLevel <= 1 && console.log(`[${ts()}] [INFO]`, ...args),
+  warn: (...args) => currentLevel <= 2 && console.warn(`[${ts()}] [WARN]`, ...args),
+  error: (...args) => currentLevel <= 3 && console.error(`[${ts()}] [ERROR]`, ...args),
 };
