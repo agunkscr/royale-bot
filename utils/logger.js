6ea9dc743f8c596b8ff500955add74d63287c693
agunkscr <71326079+agunkscr@users.noreply.github.com>
2026-05-07 16:12:15 +0700

    Add files via upload

--- utils/logger.js
+++ utils/logger.js
@@ -1,1 +1,21 @@
+const { logLevel } = require('../config');
 
+const LEVELS = { debug: 0, info: 1, warn: 2, error: 3 };
+const currentLevel = LEVELS[logLevel] || LEVELS.info;
+
+function timestamp() {
+  return new Date().toISOString();
+}
+
+function log(level, ...args) {
+  if (LEVELS[level] >= currentLevel) {
+    console.log(`[${timestamp()}] [${level.toUpperCase()}]`, ...args);
+  }
+}
+
+module.exports = {
+  debug: (...args) => log('debug', ...args),
+  info: (...args) => log('info', ...args),
+  warn: (...args) => log('warn', ...args),
+  error: (...args) => log('error', ...args),
+};
